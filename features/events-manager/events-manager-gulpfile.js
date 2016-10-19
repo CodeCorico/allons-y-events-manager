1e5ff3e4f3c22e76385e900cd82810a8cf54347c
Xavier Boubert <xavier@boubert.fr>
2016-10-19 11:53:23 +0200

    fix(events-manager gulpfile): add relative path to $gulp.src()

--- features/events-manager/events-manager-gulpfile.js
+++ features/events-manager/events-manager-gulpfile.js
@@ -4,8 +4,8 @@ module.exports = function($gulp) {
 
   $gulp.task('eventsManager', function(done) {
     $gulp.src([
-      'node_modules/events-manager/events-manager.js',
-      'node_modules/events-manager/events-manager.min.js'
+      './node_modules/events-manager/events-manager.js',
+      './node_modules/events-manager/events-manager.min.js'
     ])
       .pipe($gulp.dist('vendor'))
       .on('end', done);
